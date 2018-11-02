3f6ad04e37f049f04ef375855449d11ab191119a
thatvictor7 <montoya.victor7@gmail.com>
2018-11-02 14:03:32 -0600

    added error handlers

--- index.js
+++ index.js
@@ -22,7 +22,21 @@ app.get('/', (req,res) =>{
 })
 
 
+app.use(notFound)
+  // General purpose 'catch' all errors
+app.use(errorHandler)
 
+
+function notFound(req, res, next) {
+  res.status(404).send({ error: 'Not found!', status: 404, url: req.originalUrl })
+}
+
+// eslint-disable-next-line
+function errorHandler(err, req, res, next) {
+  console.error('ERROR', err)
+  const stack = process.env.NODE_ENV !== 'production' ? err.stack : undefined
+  res.status(500).send({ error: err.message, stack, url: req.originalUrl })
+}
 
 
 
